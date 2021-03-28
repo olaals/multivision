1e41568e867f3f46d635e750c3e4470ba4af93a4
olaals <olaalstad96@gmail.com>
2021-03-28 14:52:26 +0200

    Restructure package after renaming

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-        name='tpktools',
+        name='multivision',
         version='0.0.1',
-        description='Tools for personal project in robotics',
+        description='Tools for personal project in multiview computer vision',
         py_modules=["realAPI", "robotics", "simAPI", "sli"],
         package_dir={'':'src'},
         install_requires=['matplotlib']
