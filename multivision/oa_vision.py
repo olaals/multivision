07da846e12aa7f1ca81db1efd2089ad27206dd63
olaals <olaalstad96@gmail.com>
2021-05-08 18:35:48 +0200

    Add masking functions

--- multivision/oa_vision.py
+++ multivision/oa_vision.py
@@ -1,3 +1,23 @@
 import cv2
 from oa_robotics import *
 
+
+def row_wise_max_mask(mat, row_sum_threshold=0):
+    row_sums = np.sum(mat, axis=1, keepdims=True)>row_sum_threshold
+    mask = np.where(row_sums, mat.max(axis=1,keepdims=1) == mat, 0)
+    return mask
+
+
+def row_wise_max_index_mask(mat):
+    ind = np.indices(mat.shape)[1]
+    img_filter = np.where(mat>0, ind, 0)
+    print(img_filter)
+    img_filter = row_wise_max_mask(img_filter)
+    return img_filter
+
+def shift_add_values(mat, shift_array):
+    for shift in shift_array:
+        mat += np.roll(mat, shift)
+    return mat
+
+
