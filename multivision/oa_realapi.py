50416ca554809e3d2f364b25531c78cf4751311c
olaals <olaalstad96@gmail.com>
2021-05-30 22:41:54 +0200

    Project line real projector

--- multivision/oa_realapi.py
+++ multivision/oa_realapi.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import screeninfo
+import oa_ls
 
 def init_proj(window_name, screen_id):
     screen = screeninfo.get_monitors()[screen_id]
@@ -10,4 +11,18 @@ def init_proj(window_name, screen_id):
     cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN,
                         cv2.WINDOW_FULLSCREEN)
     return width, height
+
+def show_laser_line():
+    cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
+    width, height = init_proj("window", 1)
+    img_ls = create_laser_scan_line_speckle((0,0,255), 1, width, height, 3)
+    cv2.imshow("window", img_ls)
+    cv2.waitKey(0)
+ 
+
+if __name__ == '__main__':
+    show_laser_line()
+
+
+
 
